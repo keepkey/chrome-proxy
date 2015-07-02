4a573e02074275df99abb54f994e97f476450cff
Kenneth Heutmaker <ken@keepkey.com>
2015-07-02 14:03:49 -0700

    WIP - Added code to generate an address associated with BIP44 account. - Added bitcore.js

--- src/modules/keepkeyjs/walletNodeService.js
+++ src/modules/keepkeyjs/walletNodeService.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 var dbPromise = require('./dbPromise.js');
 var EventEmitter2 = require('eventemitter2').EventEmitter2;
+var bitcore = require('bitcore');
 
 const NODES_STORE_NAME = 'nodes';
 
@@ -8,40 +9,88 @@ var eventEmitter = new EventEmitter2();
 
 var walletNodes = {
     nodes: [],
-    addListener: eventEmitter.addListener.bind(eventEmitter),
-    registerPublicKey: function registerPublicKey(publicKeyObject) {
-        var oldNodes = JSON.parse(JSON.stringify(walletNodes.nodes));
-
-        var matches = _.filter(walletNodes.nodes, function(it) {
-            var childNum = it.nodePath[it.nodePath.length - 1];
-            var level = it.nodePath.length;
-            return childNum === publicKeyObject.node.child_num &&
-                level === publicKeyObject.node.depth;
-        });
-
-        if (matches.length !== 1) {
-            console.error('error while matching public key to a node');
-            return;
-        }
-
-        var match = matches[0];
-
-        match.xpub = publicKeyObject.xpub;
-        match.chainCode = publicKeyObject.node.chain_code.toHex();
-        match.fingerprint = publicKeyObject.node.fingerprint;
-        match.publicKey = publicKeyObject.node.public_key.toHex();
-
-        dbPromise.then(function(db) {
-            var store = db
-                .transaction(NODES_STORE_NAME, 'readwrite')
-                .objectStore(NODES_STORE_NAME);
-
-            store.put(match);
-        });
-
-        eventEmitter.emit('changed', walletNodes.nodes, oldNodes);
-    }
+    addListener: eventEmitter.addListener.bind(eventEmitter)
 };
+
+walletNodes.registerPublicKey = function registerPublicKey(publicKeyObject) {
+    var oldNodes = JSON.parse(JSON.stringify(walletNodes.nodes));
+
+    var matches = _.filter(walletNodes.nodes, function (it) {
+        var childNum = it.nodePath[it.nodePath.length - 1];
+        var level = it.nodePath.length;
+        return childNum === publicKeyObject.node.child_num &&
+            level === publicKeyObject.node.depth;
+    });
+
+    if (matches.length !== 1) {
+        console.error('error while matching public key to a node');
+        return;
+    }
+
+    var match = matches[0];
+
+    match.xpub = publicKeyObject.xpub;
+    match.chainCode = publicKeyObject.node.chain_code.toHex();
+    match.fingerprint = publicKeyObject.node.fingerprint;
+    match.publicKey = publicKeyObject.node.public_key.toHex();
+
+    populateAddresses('[0-1]/[0-19]', match);
+
+    dbPromise.then(function (db) {
+        var store = db
+            .transaction(NODES_STORE_NAME, 'readwrite')
+            .objectStore(NODES_STORE_NAME);
+
+        store.put(match);
+    });
+
+    eventEmitter.emit('changed', walletNodes.nodes, oldNodes);
+};
+
+
+function populateAddresses(range, node) {
+    if (!node.addresses) {
+        node.addresses = [];
+    }
+    scanNodes(bitcore.HDPublicKey(node.xpub), node.addresses, parseRange(range));
+}
+
+
+function scanNodes(publicKey, addresses, ranges) {
+    for (var i = ranges[0].start; i <= ranges[0].end; i++) {
+        var derivedKey = publicKey.derive(i);
+        if (ranges.length > 1) {
+            addresses[i] = scanNodes(derivedKey, addresses[i] || [], ranges.slice(1));
+        }
+        else {
+            addresses[i] = {
+                xpub: derivedKey.publicKey.toString(),
+                address: derivedKey.publicKey.toAddress().toString()
+            };
+        }
+    }
+    return addresses;
+}
+
+
+function parseRange(range) {
+    var levels = range.split('/');
+    var parsed = [];
+
+    for (var i = 0; i < levels.length; i++) {
+        var level = levels[i];
+        var tokens = level.match(/\[([0-9]+)-([0-9]+)\]/);
+        if (tokens) {
+            parsed.push({
+                start: parseInt(tokens[1]),
+                end: parseInt(tokens[2])
+            });
+        } else {
+            throw 'unable to parse range: ' + level;
+        }
+    }
+    return parsed;
+}
 
 dbPromise
     .then(function (db) {
